f1e1e29b7b01528af87bacba64739be11df3f84d
thanhnd1503 <thanhnd15032002@gmail.com>
2022-12-09 07:36:37 +0700

    Add area.html files

--- Thuc-hanh.js
+++ Thuc-hanh.js
@@ -1,26 +1,30 @@
 // Bài 1
-let i = 10
-let f = 20.5
-let b = true
-let s = 'Hà Nội'
-document.write('i = ' + i)
-document.write('<br/>')
-document.write('f = ' + f)
-document.write('<br/>')
-document.write('b = ' + b)
-document.write('<br/>')
-document.write('s = ' + s)
+// let i = 10
+// let f = 20.5
+// let b = true
+// let s = 'Hà Nội'
+// document.write('i = ' + i)
+// document.write('<br/>')
+// document.write('f = ' + f)
+// document.write('<br/>')
+// document.write('b = ' + b)
+// document.write('<br/>')
+// document.write('s = ' + s)
 // Bài 2
-let width = 20
-let height = 10
-let area = width * height
-document.write('Area = ' + area)
+// let width = 20
+// let height = 10
+// let area = width * height
+// document.write('Area = ' + area)
 // Bài 3
-let a = prompt("Enter a:");
-let b = prompt("Enter b:");
-let c = a%b;
-if(c==0){
-    alert(a + "chia het cho" + b);
-}else {
-    alert(a + "kh chia het cho"+ b);
-}
+// let a = prompt("Enter a:");
+// let b = prompt("Enter b:");
+// let c = a%b;
+// if(c==0){
+//     alert(a + "chia het cho" + b);
+// }else {
+//     alert(a + "kh chia het cho"+ b);
+// }
+
+
+
+
